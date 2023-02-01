10f24d251f6d6c69fde689b0813923f85abbb0ad
Pedro <Pedrocorreiabatista@gmail.com>
2023-02-01 16:24:16 +0100

    added small task

---
+++ exercise/index.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            tableElements: [
+                {
+                    Kursteilnehmer: 'Emanuel',
+                    Jahrgang: 1983,
+                    Kenntnisse: "HTML, CSS, PHP",
+                },
+                {
+                    Kursteilnehmer: 'Daniela',
+                    Jahrgang: 1984,
+                    Kenntnisse: "HTML, CSS, Vuejs, JS",
+                },
+                {
+                    Kursteilnehmer: 'Fabian',
+                    Jahrgang: 1985,
+                    Kenntnisse: "XML, PHP",
+                },
+                {
+                    Kursteilnehmer: 'Tanja',
+                    Jahrgang: 1986,
+                    Kenntnisse: "PHP; JS",
+                },
+                {
+                    Kursteilnehmer: 'Markus',
+                    Jahrgang: 1987,
+                    Kenntnisse: "TS",
+                }
+            ]
+        }
+    },
+});
